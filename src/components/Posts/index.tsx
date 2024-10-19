64b69d3261dd3ea7bb3f1b0794d051ddc3147e36
Matheus Oliveira <matheuseu41@gmail.com>
2024-10-19 11:38:49 -0300

    Ajustanda página de Posts para consumir os dados do BackEnd

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -7,100 +7,99 @@ import dolangue from '../../assets/dolangue.png';
 import dolanguepost from '../../assets/dolangue-post.png';
 import cesar from '../../assets/cesar.jpg';
 import cesarpost from '../../assets/cesar-post.jpg';
+import React, { useState, useEffect } from 'react';
 
-const postsData = {
-  posts: [
-    {
-      user: {
-        name: 'Dolangue News',
-        username: '@DolangueNews',
-        profileImage: dolangue,
-        verified: true
-      },
-      content: {
-        text: 'URGENTE: já é quase amanhã',
-        image: dolanguepost
-      },
-      interactions: {
-        likes: 255,
-        comments: 255
-      }
-    },
-    {
-      user: {
-        name: 'Cesar Menotti',
-        username: '@CesarMenotti',
-        profileImage: cesar,
-        verified: false
-      },
-      content: {
-        text: 'Se não for com paixão, eu nem faço ❤️',
-        image: cesarpost
-      },
-      interactions: {
-        likes: 120,
-        comments: 50
-      }
-    },
-    {
-      user: {
-        name: 'João Silva',
-        username: '@JoaoSilva22',
-        profileImage: perfil,
-        verified: false
-      },
-      content: {
-        text: 'Novo post sobre programação e tecnologia! Confira no meu blog.',
-        image: dog
-      },
-      interactions: {
-        likes: 120,
-        comments: 50
-      }
-    }
-  ]
+export type Post = {
+  id: number;
+  content: string;
+  released: string;
+  author: {
+    name: string;
+    username: string;
+    perfilPhoto: string;
+  };
+  imagens: {
+    id: number;
+    image: string;
+    post: number;
+  }[];
+  likes: number[];
+  comentarios: number[];
 };
 
-const Posts = () => (
-  <S.Container>
-    <S.Header>
-      <S.Button>For You</S.Button>
-      <S.Button>Following</S.Button>
-    </S.Header>
-    <S.PostList>
-      {postsData.posts.map((post, index) => (
-        <S.Post key={index}>
-          <S.ProfileButton>
-            <S.ProfileImage src={post.user.profileImage} alt={post.user.name} />
-            <S.Row>
-              <S.ProfileName>
-                <p>{post.user.name}</p>
-                <S.Verified className="bi bi-patch-check-fill"></S.Verified>{' '}
-                <span>{post.user.username}</span>
-                <S.More className="bi bi-three-dots" />
-              </S.ProfileName>
-              <S.TweetText>{post.content.text}</S.TweetText>
-              {post.content.image && (
-                <S.ImageDiv>
-                  <img src={post.content.image} alt="Post Content" />
-                </S.ImageDiv>
-              )}
-              <S.Actions>
-                <button>
-                  <i className="bi bi-heart"></i>
-                  <span>{post.interactions.likes}</span>
-                </button>
-                <button>
-                  <i className="bi bi-chat"></i>
-                  <span>{post.interactions.comments}</span>
-                </button>
-              </S.Actions>
-            </S.Row>
-          </S.ProfileButton>
-        </S.Post>
-      ))}
-    </S.PostList>
-  </S.Container>
-);
+const Posts = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/feed/posts/')
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar os posts:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Button>For You</S.Button>
+        <S.Button>Following</S.Button>
+      </S.Header>
+      <S.PostList>
+        {posts.map((post) => (
+          <S.Post key={post.id}>
+            <S.ProfileButton>
+              <S.ProfileImage
+                // src={dolangue}
+                src={post.author.perfilPhoto}
+                // alt={post.author.name}
+              />
+              <S.Row>
+                <S.ProfileName>
+                  <p>{post.author.name}</p>
+                  <S.Verified className="bi bi-patch-check-fill"></S.Verified>{' '}
+                  {/* <span>{post.user.username}</span> */}
+                  <span>{post.author.username}</span>
+                  <S.More className="bi bi-three-dots" />
+                </S.ProfileName>
+                <S.TweetText>{post.content}</S.TweetText>
+                {post.imagens.length > 0 && (
+                  <S.ImageDiv>
+                    {post.imagens.map((imagem) => (
+                      <img
+                        key={imagem.id}
+                        src={imagem.image}
+                        alt="Post Content"
+                      />
+                    ))}
+                  </S.ImageDiv>
+                )}
+                <S.Actions>
+                  <button>
+                    <i className="bi bi-heart"></i>
+                    <span>{post.likes.length}</span>
+                  </button>
+                  <button>
+                    <i className="bi bi-chat"></i>
+                    <span>{post.comentarios.length}</span>
+                  </button>
+                </S.Actions>
+              </S.Row>
+            </S.ProfileButton>
+          </S.Post>
+        ))}
+      </S.PostList>
+    </S.Container>
+  );
+};
 
 export default Posts;
